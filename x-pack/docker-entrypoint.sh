99435df9129d97fe67e25c4f9d0bb7ad867704ab
blacktop <blacktop@users.noreply.github.com>
2018-03-04 14:57:22 -0700

    fixes

--- x-pack/docker-entrypoint.sh
+++ x-pack/docker-entrypoint.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -e
 
+declare -a kb_opts
+
+while IFS='=' read -r envvar_key envvar_value
+do
+    # Elasticsearch env vars need to have at least two dot separated lowercase words, e.g. `cluster.name`
+    if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ ]]; then
+        if [[ ! -z $envvar_value ]]; then
+          kb_opt="--${envvar_key}=${envvar_value}"
+          kb_opts+=("${kb_opt}")
+        fi
+    fi
+done < <(env)
+
 # Add kibana as command if needed
 if [[ "$1" == -* ]]; then
 	set -- kibana "$@"
 fi
 
 # Run as user "kibana" if the command is "kibana"
-if [ "$1" = 'kibana' ]; then
-	if [ "$ELASTICSEARCH_URL" ]; then
-		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /usr/share/kibana/config/kibana.yml
-	fi
-
-	set -- su-exec kibana "$@"
+if [ "$1" = 'kibana' -a "$(id -u)" = '0' ]; then
+	echo "$@" "${kb_opts[@]}"
+	set -- su-exec kibana "$@" "${kb_opts[@]}"
 fi
 
 exec "$@"
